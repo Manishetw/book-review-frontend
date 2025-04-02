02aa2c5b8332014a8390d4ff4cdc1b80f431b21d
shiwanibaghel76 <shiwanibaghel76@gmail.com>
2025-04-02 12:05:02 +0530

    background images

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,11 @@
+
+import bookImage from '../assets/images/backgroundimage5.jpg';
 const Home = () => {
     return (
         <div>
         <h1>Welcome to the Book Review App</h1>
         <p>Discover and review your favorite books!</p>
+         image:backgroundimage5,
         </div>
     );
 }
